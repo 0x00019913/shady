768d6d09807aea6e275962016a94a410018d446e
Anton Bovin <0x00019913@gmail.com>
2017-03-22 21:04:48 -0700

    added gui, enabled saving shaders

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var width, height;
 var container, camera, scene, renderer, controls;
-var mesh;
+var mesh, offset, offsetRateX, offsetRateY;
+var gui;
 container = document.getElementById('container');
 
 init();
@@ -15,6 +16,13 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0);
 
+  offset = new THREE.Vector2();
+  offsetRateX = 0.0, offsetRateY = 0.0;
+  gui = new dat.GUI();
+  gui.add(this, "offsetRateX");
+  gui.add(this, "offsetRateY");
+  gui.add(this, "resetOffset");
+
   controls = new Controls(
     camera,
     container,
@@ -28,13 +36,12 @@ function init() {
 
   var planeGeo = new THREE.PlaneGeometry(1,1);
   mesh = new THREE.Mesh(planeGeo);
-  updateMaterial();
   mesh.rotateY(Math.PI/2);
+  updateMaterial();
   scene.add(mesh);
 
   /* RENDER */
   renderer = new THREE.WebGLRenderer({ antialias: true });
-  //renderer.toneMapping = THREE.ReinhardToneMapping;
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
   container.appendChild(renderer.domElement);
@@ -62,11 +69,16 @@ function animate() {
 function render() {
   if (!camera || !scene) return;
   controls.update();
+  offset.x += offsetRateX;
+  offset.y += offsetRateY;
   renderer.render(scene, camera);
 }
 
 function updateMaterial() {
   var material = new THREE.ShaderMaterial({
+    uniforms: {
+      offset: { value: offset }
+    },
     vertexShader: document.getElementById("vertShader").value,
     fragmentShader: document.getElementById("fragShader").value,
     side: THREE.DoubleSide
@@ -75,3 +87,32 @@ function updateMaterial() {
 }
 
 document.getElementById("runButton").onclick = updateMaterial;
+document.getElementById("saveVertButton").onclick = function() { saveShader("vertex"); }
+document.getElementById("saveFragButton").onclick = function() { saveShader("fragment"); }
+
+function saveShader(type) {
+  var fname = type+"Shader.txt";
+  var source = document.getElementById(type=="vertex" ? "vertShader" : "fragShader");
+  var blob = new Blob([source.value], { type: 'text/plain' });
+
+  var a = document.createElement("a");
+  if (window.navigator.msSaveOrOpenBlob) { // IE :(
+    window.navigator.msSaveOrOpenBlob(blob, fname);
+  }
+  else {
+    var url = URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fname;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    });
+  }
+}
+
+function resetOffset() {
+  offset.x = 0;
+  offset.y = 0;
+}
