5fb32b095640bfa8427d3e5371a4e9b5435b7bba
Anton Bovin <0x00019913@gmail.com>
2017-03-30 01:27:10 -0500

    can now load shaders

--- js/main.js
+++ js/main.js
@@ -24,6 +24,11 @@ function init() {
   gui.add(this, "saveVertShader");
   gui.add(this, "saveFragShader");
   gui.add(this, "resetCamera");
+  var loadFolder = gui.addFolder("Load");
+  loadFolder.add(this, "loadDefault");
+  loadFolder.add(this, "loadSimplexNoise");
+  loadFolder.add(this, "loadFBMNoise");
+  loadFolder.add(this, "loadMovingClouds");
   var meshFolder = gui.addFolder("Mesh");
   meshFolder.add(this, "setMeshPlane");
   meshFolder.add(this, "setMeshSphere");
@@ -57,6 +62,8 @@ function init() {
     }
   );
 
+
+  loadDefault();
   setMeshPlane();
 
   var bgVert = "\
@@ -151,12 +158,15 @@ function setMeshSphere() {
 }
 
 function updateShader() {
+  if (!mesh) return;
+  var vert = document.getElementById("vertShader").value;
+  var frag = document.getElementById("fragShader").value;
   var material = new THREE.ShaderMaterial({
     uniforms: {
       offset: { value: offset }
     },
-    vertexShader: document.getElementById("vertShader").value,
-    fragmentShader: document.getElementById("fragShader").value,
+    vertexShader: vert,
+    fragmentShader: frag,
     side: THREE.DoubleSide
   });
   mesh.material = material;
@@ -203,6 +213,11 @@ function resetOffsetRates() {
   for (var i=0; i<offsetRateControls.length; i++) offsetRateControls[i].updateDisplay();
 }
 
+function resetAnimation() {
+  resetOffsetRates();
+  resetOffset();
+}
+
 document.getElementById("vertShader").addEventListener("keydown", handleKeyDownVert, false);
 document.getElementById("fragShader").addEventListener("keydown", handleKeyDownFrag, false);
 function handleKeyDownVert(e) { handleKeyDown("vertex", e); }
@@ -222,4 +237,314 @@ function handleKeyDown(type, e) {
     e.preventDefault();
     updateShader();
   }
+}
+
+function loadDefault() {
+  resetAnimation();
+
+  var default_vert = "\
+  /* vertex shader */\n\
+  varying vec3 vPos;\n\
+  \n\
+  void main() {\n\
+    vPos = position;\n\
+    vPos += 0.5; /* positions in unit plane go from -0.5 to 0.5 */\n\
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n\
+  }";
+  var default_frag = "\
+  /* fragment shader */\n\
+  uniform vec3 offset;\n\
+  \n\
+  varying vec3 vPos;\n\
+  \n\
+  void main() {\n\
+    gl_FragColor = vec4(vPos.xy+offset.xy, 1.0, 1.0);\n\
+  }";
+  document.getElementById("vertShader").value = default_vert;
+  document.getElementById("fragShader").value = default_frag;
+  updateShader();
+}
+
+function loadSimplexNoise() {
+  resetAnimation();
+
+  var simplex_noise_vert = "\
+  /* vertex shader */\n\
+  varying vec3 vPos;\n\
+  \n\
+  void main() {\n\
+    vPos = position;\n\
+    vPos += 0.5;\n\
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n\
+  }";
+  var simplex_noise_frag = "\
+  /* fragment shader */\n\
+  varying vec3 vPos;\n\
+  \n\
+  vec3 mod289(vec3 x) {\
+    return x - floor(x * (1.0/289.0)) * 289.0;\n\
+  }\n\
+  \n\
+  vec3 permute(vec3 x) {\n\
+    return mod289(((x*34.0) + 1.0) * x);\n\
+  }\n\
+  \n\
+  const float F2 = 0.5*(sqrt(3.0)-1.0);\n\
+  const float G2 = (3.0-sqrt(3.0))/6.0;\n\
+  \n\
+  float simplex(vec2 v) {\n\
+  \n\
+    /* base corner */\n\
+    vec2 i = floor(v + (v.x+v.y)*F2);\n\
+    vec2 x0 = v - i + (i.x+i.y)*G2;\n\
+  \n\
+    /* middle and far corners */\n\
+    vec2 i1;\n\
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);\n\
+    vec2 x1 = x0 - i1 + G2;\n\
+    vec2 x2 = x0 - 1.0 + 2.0*G2;\n\
+  \n\
+    i = mod289(vec3(i, 0.0)).xy;\n\
+    vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));\n\
+  \n\
+    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);\n\
+    m = m*m;\n\
+    m = m*m;\n\
+  \n\
+    vec3 x = 2.0 * fract(p / 41.0) - 1.0;\n\
+    vec3 h = abs(x) - 0.5;\n\
+    vec3 ox = floor(x + 0.5);\n\
+    vec3 a0 = x-ox;\n\
+  \n\
+    m *= 1.79284291400159 - 0.85373472095314 * (a0*a0 + h*h);\n\
+  \n\
+    vec3 g;\n\
+    g.x = a0.x*x0.x + h.x*x0.y;\n\
+    g.y = a0.y*x1.x + h.y*x1.y;\n\
+    g.z = a0.z*x2.x + h.z*x2.y;\n\
+  \n\
+    float intensity = 130.0 * dot(m,g);\n\
+    return intensity;\n\
+  }\n\
+  \n\
+  void main() {\n\
+    float intensity = simplex(vPos.xy*2.5);\n\
+    vec3 level = clamp(vec3(intensity), 0.0, 1.0);\n\
+    gl_FragColor = vec4(level, 1.0);\n\
+  }";
+  document.getElementById("vertShader").value = simplex_noise_vert;
+  document.getElementById("fragShader").value = simplex_noise_frag;
+  updateShader();
+}
+
+function loadFBMNoise() {
+  resetAnimation();
+
+  var FBM_noise_vert = "\
+  /* vertex shader */\n\
+  varying vec3 vPos;\n\
+  \n\
+  void main() {\n\
+    vPos = position;\n\
+    vPos += 0.5;\n\
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n\
+  }";
+  var FBM_noise_frag = "\
+  /* fragment shader */\n\
+  varying vec3 vPos;\n\
+  \n\
+  vec3 mod289(vec3 x) {\
+    return x - floor(x * (1.0/289.0)) * 289.0;\n\
+  }\n\
+  \n\
+  vec3 permute(vec3 x) {\n\
+    return mod289(((x*34.0) + 1.0) * x);\n\
+  }\n\
+  \n\
+  const float F2 = 0.5*(sqrt(3.0)-1.0);\n\
+  const float G2 = (3.0-sqrt(3.0))/6.0;\n\
+  \n\
+  float simplex(vec2 v) {\n\
+  \n\
+    /* base corner */\n\
+    vec2 i = floor(v + (v.x+v.y)*F2);\n\
+    vec2 x0 = v - i + (i.x+i.y)*G2;\n\
+  \n\
+    /* middle and far corners */\n\
+    vec2 i1;\n\
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);\n\
+    vec2 x1 = x0 - i1 + G2;\n\
+    vec2 x2 = x0 - 1.0 + 2.0*G2;\n\
+  \n\
+    i = mod289(vec3(i, 0.0)).xy;\n\
+    vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));\n\
+  \n\
+    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);\n\
+    m = m*m;\n\
+    m = m*m;\n\
+  \n\
+    vec3 x = 2.0 * fract(p / 41.0) - 1.0;\n\
+    vec3 h = abs(x) - 0.5;\n\
+    vec3 ox = floor(x + 0.5);\n\
+    vec3 a0 = x-ox;\n\
+  \n\
+    m *= 1.79284291400159 - 0.85373472095314 * (a0*a0 + h*h);\n\
+  \n\
+    vec3 g;\n\
+    g.x = a0.x*x0.x + h.x*x0.y;\n\
+    g.y = a0.y*x1.x + h.y*x1.y;\n\
+    g.z = a0.z*x2.x + h.z*x2.y;\n\
+  \n\
+    float intensity = 130.0 * dot(m,g);\n\
+    return intensity;\n\
+  }\n\
+  float fbm(vec2 v, float lacunarity, float gain) {\n\
+    const int octaves = 5;\n\
+    float sum = 0.0;\n\
+    float amp = 1.0;\n\
+    float freq = 1.0;\n\
+    /* Fun story. In my Ubuntu VM, putting the simplex() call in a loop causes\n\
+       the shader to render black every time. Works fine on Windows. Why, GLSL,\n\
+       why? So I unwrapped it to 7 octaves. */\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    return sum;\n\
+  }\n\
+  \n\
+  void main() {\n\
+    float intensity = 0.70*fbm(vPos.xy*2.5, 2.0, 0.5);\n\
+    vec3 level = clamp(vec3(intensity), 0.0, 1.0);\n\
+    gl_FragColor = vec4(level, 1.0);\n\
+  }";
+  document.getElementById("vertShader").value = FBM_noise_vert;
+  document.getElementById("fragShader").value = FBM_noise_frag;
+  updateShader();
+}
+
+function loadMovingClouds() {
+  resetAnimation();
+  offsetRateY = 0.001;
+
+  var clouds_vert = "\
+  /* vertex shader */\n\
+  varying vec3 vPos;\n\
+  \n\
+  void main() {\n\
+    vPos = position;\n\
+    vPos += 0.5;\n\
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n\
+  }";
+  var clouds_frag = "\
+  /* fragment shader */\n\
+  uniform vec3 offset;\n\
+  \n\
+  varying vec3 vPos;\n\
+  \n\
+  vec3 mod289(vec3 x) {\
+    return x - floor(x * (1.0/289.0)) * 289.0;\n\
+  }\n\
+  \n\
+  vec3 permute(vec3 x) {\n\
+    return mod289(((x*34.0) + 1.0) * x);\n\
+  }\n\
+  \n\
+  const float F2 = 0.5*(sqrt(3.0)-1.0);\n\
+  const float G2 = (3.0-sqrt(3.0))/6.0;\n\
+  \n\
+  float simplex(vec2 v) {\n\
+  \n\
+    /* base corner */\n\
+    vec2 i = floor(v + (v.x+v.y)*F2);\n\
+    vec2 x0 = v - i + (i.x+i.y)*G2;\n\
+  \n\
+    /* middle and far corners */\n\
+    vec2 i1;\n\
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);\n\
+    vec2 x1 = x0 - i1 + G2;\n\
+    vec2 x2 = x0 - 1.0 + 2.0*G2;\n\
+  \n\
+    i = mod289(vec3(i, 0.0)).xy;\n\
+    vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));\n\
+  \n\
+    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);\n\
+    m = m*m;\n\
+    m = m*m;\n\
+  \n\
+    vec3 x = 2.0 * fract(p / 41.0) - 1.0;\n\
+    vec3 h = abs(x) - 0.5;\n\
+    vec3 ox = floor(x + 0.5);\n\
+    vec3 a0 = x-ox;\n\
+  \n\
+    m *= 1.79284291400159 - 0.85373472095314 * (a0*a0 + h*h);\n\
+  \n\
+    vec3 g;\n\
+    g.x = a0.x*x0.x + h.x*x0.y;\n\
+    g.y = a0.y*x1.x + h.y*x1.y;\n\
+    g.z = a0.z*x2.x + h.z*x2.y;\n\
+  \n\
+    float intensity = 130.0 * dot(m,g);\n\
+    return intensity;\n\
+  }\n\
+  float fbm(vec2 v, float lacunarity, float gain) {\n\
+    const int octaves = 5;\n\
+    float sum = 0.0;\n\
+    float amp = 1.0;\n\
+    float freq = 1.0;\n\
+    /* Fun story. In my Ubuntu VM, putting the simplex() call in a loop causes\n\
+       the shader to render black every time. Works fine on Windows. Why, GLSL,\n\
+       why? So I unwrapped it to 7 octaves. */\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    sum += amp * simplex(v * freq);\n\
+    freq *= lacunarity;\n\
+    amp *= gain;\n\
+    return sum;\n\
+  }\n\
+  \n\
+  void main() {\n\
+    vec3 pos = vPos;\n\
+    pos.y /= 2.0;\n\
+    float intensity = 0.70*fbm((pos + offset).xy * 4.0, 2.0, 0.5);\n\
+    float noiseSub = 0.2*fbm((pos + offset*0.81).xy * 24.0, 2.0, 0.5);\n\
+    vec3 level = clamp(vec3(intensity-noiseSub), 0.0, 1.0);\n\
+    gl_FragColor = vec4(level, 1.0);\n\
+  }";
+  document.getElementById("vertShader").value = clouds_vert;
+  document.getElementById("fragShader").value = clouds_frag;
+  updateShader();
 }
